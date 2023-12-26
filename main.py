e378fd3079e89583f68d2a443637f071f844f2c6
Ivan <ivankruzhaev@yandex.ru>
2023-12-26 20:41:46 +0700

    start create dialogs

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
 import sys
-
+from random import choice
 import pygame
 from pygame.time import Clock
-
-FPS = 50
 
 
 def load_image(name, colorkey=None):
@@ -33,6 +31,31 @@ def start_screen():
         text_coord += 50
 
 
+def game_screen():
+    fon = pygame.transform.scale(load_image('coffee_bg.png'), (width, height))
+    screen.blit(fon, (0, 0))
+    guy = load_image('guy3.png')
+    screen.blit(guy, (220, 68))
+    dialog = Dialog(ordering())
+
+
+def ordering():
+    coffee_types = ['эспрессо', 'американо', 'капучино', 'латте', 'раф', 'моккачино', 'фраппучино']
+    desserts = ['шоколадный кекс', 'ванильный кекс', 'черничный кекс',
+                'торт "Наполеон"', 'торт "Медовик"', 'торт "Красный бархат"', 'торт "Птичье молоко"',
+                'круассан с шоколадом', 'круассан с вареной сгущенкой', 'круассан с джемом', 'круассан классический',
+                'булочка с корицей', 'булочка "Синнабон"', None, None]
+    # None нужен для того, чтобы не всегда была вкусняшка
+    greetings = [' - Здравствуйте! Можно мне ', ' - Здравствуйте! Мне пожалуйста ', ' - Здравствуйте! Я бы хотел ',
+                 ' - Добрый день! Мне пожалуйста ', ' - Добрый день! Можно мне ', ' - Добрый день! Я бы хотел ']
+    coffee = choice(coffee_types)
+    dessert = choice(desserts)
+    order = [choice(greetings) + coffee]
+    if dessert is not None:
+        order.append(f'и {dessert}')
+    return order
+
+
 class Button(pygame.sprite.Sprite):
     def __init__(self, text, text_coord):
         super().__init__(buttons_group, all_sprites)
@@ -41,23 +64,46 @@ class Button(pygame.sprite.Sprite):
         self.rect = pygame.Rect(20, text_coord, 150, 30)
         self.font = pygame.font.Font(None, 40)
         pygame.draw.rect(self.image, (247, 214, 171), (0, 0, 150, 30))
-        self.text = self.font.render(text, 0, (245, 190, 118))
+        self.text = self.font.render(self.btn_text, 0, (204, 152, 102))
         self.image.blit(self.text, (2, 2))
         screen.blit(self.image, self.rect)
 
     def update(self, *args):
         if self.rect.collidepoint(args[0].pos):
             if self.btn_text == 'New game':
-                print('game started')
+                game_screen()
             if self.btn_text == 'Continue':
                 print('game continued')
             if self.btn_text == 'Instruct':
                 print('instruct is opened')
 
 
+class Dialog(pygame.sprite.Sprite):
+    def __init__(self, text):
+        super().__init__(all_sprites)
+        self.text = text[::]
+        self.size = (500, 10 + 15 * len(self.text) + 10)
+        self.image = pygame.Surface(self.size, pygame.SRCALPHA, 32)
+        self.rect = pygame.Rect(100, 300, *self.size)
+        self.font = pygame.font.Font(None, 20)
+        pygame.draw.rect(self.image, (242, 151, 65), (0, 0, *self.size))
+        pygame.draw.rect(self.image, (252, 199, 149), (3, 3, self.size[0] - 6, self.size[1] - 6))
+        self.text_coord = 10
+        for line in self.text:
+            string_rendered = self.font.render(line, 1, (89, 58, 29))
+            intro_rect = string_rendered.get_rect()
+            intro_rect.top = self.text_coord
+            intro_rect.x = 10
+            self.text_coord += intro_rect.height
+            self.image.blit(string_rendered, intro_rect)
+            self.text_coord += 2
+        screen.blit(self.image, self.rect)
+
+
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 720, 405
+    fps = 50
     screen = pygame.display.set_mode(size)
     clock = Clock()
     all_sprites = pygame.sprite.Group()
@@ -70,6 +116,5 @@ if __name__ == '__main__':
                 terminate()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 buttons_group.update(event)
-        all_sprites.draw(screen)
         pygame.display.flip()
-        clock.tick(FPS)
+        clock.tick(fps)
