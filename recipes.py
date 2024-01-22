e02f73e9783d798e310cdb3ca6ee0be70aaee2d7
KlLL0K <145212871+KlLL0K@users.noreply.github.com>
2024-01-22 20:33:04 +0300

    Add files via upload

---
+++ recipes.py
@@ -0,0 +1,6 @@
+recipes_lvl1 = {'espresso': [0, 0, 0, 0, 100], 'latte': [0, 0, 100, 0, 75],
+                'cappuccino': [0, 0, 75, 0, 100]}
+recipes_lvl2 = {'cherry_cappuccino': [0, 100, 60, 0, 100], 'cherry_latte': [0, 100, 100, 0, 75],
+                'mango_espresso': [100, 0, 0, 0, 100], 'raf': [0, 0, 60, 60, 30]}
+recipes_lvl3 = {'cherry_raf': [0, 100, 60, 60, 30], 'mango_raf': [100, 0, 60, 60, 30],
+                'mango_cherry_raf': [100, 100, 60, 60, 30]}
